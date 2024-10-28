2645e9d86137feb6b8b182f446457fe242d94e78
Holden <ht0726@uw.edu>
2024-10-27 20:20:46 -0700

    started the folders and basic files for model and tests

---
+++ Dungeon_Adventures/src/model/DungeonCharacter.java
@@ -0,0 +1,11 @@
+package model;
+
+public interface DungeonCharacter {
+    String getName();
+    int getHealth();
+    int getSpeed();
+    void setHealth(final int theHealth);
+    void setDamage(final int theDamage);
+    void setSpeed(final int theSpeed);
+}
+
