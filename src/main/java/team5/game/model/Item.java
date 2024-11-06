c707ca0332e34e131157c3c17117d6ab88474efc
NathanW <wanjongkhumnathan@gmail.com>
2024-11-05 17:03:06 -0800

    Fixed Itemtype error

--- src/main/java/team5/game/model/Item.java
+++ src/main/java/team5/game/model/Item.java
@@ -5,6 +5,11 @@ package team5.game.model;
  * Item is an interface that all items must implement.
  */
 public interface Item {
+    /** The type of item */
+    enum ItemType {
+        HEALTH_POTION, VISION_POTION;
+    }
+
     /** The name of the item */
     public String myName = "";
 }
